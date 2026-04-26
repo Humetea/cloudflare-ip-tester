6b1d29f62d5ec170915998ae61af8ae0f8b184a4
Humetea <49028282+Humetea@users.noreply.github.com>
2026-04-26 22:25:06 +0800

    Refactor script.js for improved readability and structure

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ options = {
             select_0()
     },
     columns: [
-        { title: "IP 地址", field: "ip" ,width: 120},
+        { title: "IP 地址", field: "ip" ,width: 200},
         { title: "机房位置（colo）", field: "region" ,width: 180},
         {
             title: "响应时间", field: "time", sorter: "number", sorterParams: {
